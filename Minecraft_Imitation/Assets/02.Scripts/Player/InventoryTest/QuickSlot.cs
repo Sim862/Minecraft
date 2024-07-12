67ada12c8f1cad0625684d3c2c361fec75fa7c50
KwonSiyoon <140003447+KwonSiyoon@users.noreply.github.com>
2024-07-12 15:10:39 +0900

    0712 Fix Debug

--- Minecraft_Imitation/Assets/02.Scripts/Player/InventoryTest/QuickSlot.cs
+++ Minecraft_Imitation/Assets/02.Scripts/Player/InventoryTest/QuickSlot.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class QuickSlot : MonoBehaviour
 {
     ItemImage item;
+    bool itemExisted;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +19,15 @@ public class QuickSlot : MonoBehaviour
         {
             item = GetComponentInChildren<ItemImage>();
             item.isPopup = true;
+        }
+        if(transform.childCount == 0 && item != null)
+        {
+            itemExisted = true;
+            if (itemExisted)
+            {
+                item.particleObjectTr.gameObject.SetActive(false);
+                itemExisted = false;
+            }
         }
     }
 }
