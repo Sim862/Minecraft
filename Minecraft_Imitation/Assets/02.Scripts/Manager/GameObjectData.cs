78d46cb025b82733a1144e591cb3253139ea6c16
Sim <smtlacg11@gmail.com>
2024-07-15 09:55:00 +0900

    Stash

--- Minecraft_Imitation/Assets/02.Scripts/Manager/GameObjectData.cs
+++ Minecraft_Imitation/Assets/02.Scripts/Manager/GameObjectData.cs
@@ -131,6 +131,7 @@ public class ObjectParticleData : GameObjectData
         WoodenPlank,
 
     //  Item  ----------------------------------------------------------------------- 
+        Stick,
         Knife,
         Ax,
         Shovel,
